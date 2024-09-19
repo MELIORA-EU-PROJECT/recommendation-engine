16ac05a260f74421b958d58b8a4e85ad52126366
NikosGour <nikosgournak@gmail.com>
2024-09-19 06:59:20 +0300

    fix some of the interventional items

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ from utils import *
 intervention_library = {"Twitter_Post_About_Quiting_Smoking": {"beh": ["cease_smoking"], "stg": [1,2,3], "opr": "max"},
                         "Youtube_Video_About_Quiting_Smoking": {"beh": ["cease_smoking"], "stg": [2,3], "opr": "max"},
                         "News_Article_About_Diet_And_Exercise": {"beh": ["improve_diet_quality", "increase_physical_activity"], "stg": [2, 3], "opr": "min"},
-                        "Facebook_Post_About_Getting_Better_Sleep": {"beh": ["improve_sleep_quality"], "stg": [4,5], "opr": "max"},
                         "Instagram_Reel_About_Reducing_Alcohol_Intake": {"beh": ["reduce_alcohol_consumption"], "stg": [2,3,4], "opr": "max"},
                         "TikTok_About_Reducing_Alcohol_Intake": {"beh": ["reduce_alcohol_consumption"], "stg": [5], "opr": "max"},
                         "Youtube_Short_About_Quiting_Smoking": {"beh": ["cease_smoking"], "stg": [1,2], "opr": "max"},
-                        "Youtube_Video_About_Improving_Life_Quality": {"beh": ["cease_smoking","reduce_alcohol_consumption","improve_diet_quality","increase_physical_activity","improve_sleep_quality"], "stg": [1,2,3], "opr": "min"},}
+                        "Youtube_Video_About_Improving_Life_Quality": {"beh": ["cease_smoking","reduce_alcohol_consumption","improve_diet_quality","increase_physical_activity","improve_mental_health"], "stg": [1,2,3], "opr": "min"},}
 # @formatter:on
 
 def get_recommendations(user_profile) -> dict:
@@ -78,19 +77,19 @@ async def root(item: Request):
         json5.dump(user_profile, write_file, indent=4, quote_keys=True)
     print(f"Aggregated Data Layer: {user_profile}")
     # TTM stages
-    user_profile = add_ttm_stages(user_profile)
+    ttm_user_profile = add_ttm_stages(user_profile)
     with open("example_patient_ttm.json", "w") as write_file:
-        json5.dump(user_profile, write_file, indent=4, quote_keys=True)
-    print(f"TTM Stages: {user_profile}")
+        json5.dump(ttm_user_profile, write_file, indent=4, quote_keys=True)
+    print(f"TTM Stages: {ttm_user_profile}")
     # Similarity Needs
-    user_profile = {k: {"str": min_max_transform(v["str"], 1, 5), "stg": v["stg"]}
-                    for k, v in user_profile.items()}
-    sim_needs = sim_need(user_profile, intervention_library)
-    sim_stages = sim_stage(user_profile, intervention_library)
+    ttm_user_profile = {k: {"str": min_max_transform(v["str"], 1, 5), "stg": v["stg"]}
+                        for k, v in ttm_user_profile.items()}
+    sim_needs = sim_need(ttm_user_profile, intervention_library)
+    sim_stages = sim_stage(ttm_user_profile, intervention_library)
     sim_totals = sim_total(sim_needs, sim_stages)
     sim_totals_filtered = {k: v for k, v in sim_totals.items() if v >= 0.5}
     sim_total_ordered = dict(sorted(sim_totals_filtered.items(), key=lambda x: x[1], reverse=True))
-    return sim_total_ordered
+    return {"recommendations": sim_total_ordered, "user_profile": ttm_user_profile}
 
 
 if __name__ == "__main__":
