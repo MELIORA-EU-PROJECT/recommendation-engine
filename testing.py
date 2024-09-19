a471e4aee0e89c243ab072be02ec0a0b4eeea467
NikosGour <nikosgournak@gmail.com>
2024-09-19 04:36:43 +0300

    testing fastapi

--- testing.py
+++ testing.py
@@ -1,24 +1,28 @@
 import random
+
+import json5
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # inverse lognormal distribution from 1 to 5
-# distribution = np.random.lognormal(3.6, 0.6, 1000)
-# distribution = np.clip(distribution, 0, 100)
-# # plt.hist(distribution, bins=100)
-# distribution = -distribution
-# distribution = distribution + 100
-# distribution = np.round(distribution)
-# distribution = distribution / (100 / 5)
-# distribution = distribution.astype(int)
-#
-# plt.hist(distribution, bins=100)
-# plt.legend(["Lognormal Distribution", "Inverse Lognormal Distribution"])
-# plt.show()
+from fastapi import FastAPI, Body, Request
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
-distribution = np.random.normal(3, 1.4, 1000)
-distribution = np.round(distribution)
-distribution = np.clip(distribution, 0, 6)
-distribution = distribution.astype(int)
-plt.hist(distribution, bins=100)
-plt.show()
+app = FastAPI()
+
+
+# class Item(BaseModel):
+#     onoma: str
+#
+
+@app.post("/")
+async def root(my_item_param: Request):
+    # async def root(my_item_param: Item):
+    print(f"{await my_item_param.json()=}")
+    # print(f"{my_item_param=}")
+    to_json = await my_item_param.json()
+    return f"Hello {to_json}"
+
+# plt.hist(distribution, bins=100)
+# plt.show()
